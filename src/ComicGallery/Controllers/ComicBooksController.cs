b027ea83bae3d8a0d379acea192c03ca585a0da8
pelino <pelmut2000@gmail.com>
2018-08-28 09:34:02 +0200

    Added Detail Action method

--- src/ComicGallery/Controllers/ComicBooksController.cs
+++ src/ComicGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,10 @@ namespace ComicGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return " Hello From the comic books controller";
+        }
+
     }
 }
